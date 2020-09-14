aabefa386cdf03e6f134222e282baec74666c097
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:15:49 +0300

    LoginForm

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = Console.ReadLine();
+
+            if (login == "admin" && password == "pass1234")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials. Try Again.");
+            }   
+
+        }
+    }
+}
---
+++ Week3LogicalOperators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Week3LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Login form
+             * login: admin
+             * password: pass1234
+            */
+            string login;
+            string password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = Console.ReadLine();
+
+            if ((login == "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Welcome");
+            }
+            else if ((login == "admin") && (password != "pass1234"))
+            {
+                Console.WriteLine("Invalid password");
+            }
+            else if ((login != "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Invalid username");
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials");
+            }
+
+
+
+            
+
+
+
+        }
+    }
+}
